544b66eac157f4f9847345bed2b2fd27d63070e7
Ramkalyan7 <yramkalyan0707@gmail.com>
2025-11-05 19:58:33 +0530

    removed unnecessary logs

--- apps/web/context/socketContex.tsx
+++ apps/web/context/socketContex.tsx
@@ -47,7 +47,6 @@ export const WebSocketProvider = ({
   useEffect(() => {
     console.log("WebSocketProvider mounted");
     const wsUrl = process.env.NEXT_PUBLIC_WS_SERVER_BASE_URL as string;
-    console.log("wsURl", wsUrl);
 
     if (!ws.current) {
       ws.current = new WebSocket(wsUrl);
